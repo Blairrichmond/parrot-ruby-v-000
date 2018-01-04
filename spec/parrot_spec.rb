1f8085e2e6aabe54320b8e5b339daf9efb1bccbc
Brent  Ransom <sadumedusa@gmail.com>
2018-01-04 14:00:31 +0000

    automatically backed up by learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -8,7 +8,6 @@ describe '#parrot' do
   end
 
   it 'should return the default phrase, "Squawk!" when called without any arguments' do
-    phrase = parrot
     expect(phrase).to eq("Squawk!")
   end
 
